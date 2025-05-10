3b6aca1ce62944e02689f21107fa1931947fcf7b
Sanskar Singh <your-correct-email@example.com>
2025-05-10 14:42:27 +0530

    added

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-          <h2 className="text-xl font-bold">Skillop</h2>
+          <h2 className="text-xl font-bold">HireUp</h2>
             <p className="text-sm">© {new Date().getFullYear()} Your Company. All rights reserved.</p>
 
           </div>
