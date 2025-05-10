3de82d676913cc10430ebef21909980538aedcae
Sanskar Singh <your-correct-email@example.com>
2025-05-10 14:47:00 +0530

    added feature

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>Skill<span className='text-[#6A38C2]'>op</span></h1>
+                    <h1 className='text-2xl font-bold'>Hire<span className='text-[#6A38C2]'>Up</span></h1>
                 </div>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
